c140d9bceff26434dcfb7a291a3d03bae566bcc5
jihoon <wlgns2223@gmail.com>
2021-08-17 17:09:17 +0900

    commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const jsonServer = require("json-server");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
+console.log(__dirname);
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname + "/../build"),
+});
+
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON server is running...");
+});
